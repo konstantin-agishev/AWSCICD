ccd85e32d3095e18a79a8d929dd65e0c7288a092
Vasiliy Petrov <xxx@yyy.zzz>
2020-06-07 11:57:41 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>My Website Home Page</title>
+</head>
+<body bgcolor=black>
+  <center>
+  <h1>Welcome to my website</h1>
+  <p><font color=red>From GitHub to S3 by EBS</p>
+  </center>
+</body>
+</html>
+
